32b70e11cf551582c67bf3c4bbd08919805dc772
jeheonee <shout_world@naver.com>
2022-07-21 21:53:43 +0900

    딕셔너리, set  중복제거 등등

---
+++ problems/Level 1/신고 결과 받기.py
@@ -0,0 +1,12 @@
+def solution(id_list, report, k):
+    reported = {a : 0 for a in id_list}
+    id_count = [0] * len(id_list)
+    
+    report = list(set(report))
+    for x in report:
+        reported[x.split()[1]] += 1
+        
+    for x in report:
+        if reported[x.split()[1]] >= k:
+            id_count[id_list.index(x.split()[0])] += 1
+    return id_count
